d703143055498c80ea1c4ed4649b5137c2d1ded6
HV240081 <hv240081@alumno.ebd.edu.sv>
2025-03-03 10:46:33 -0600

    Desarrollo y estructura de base de proyecto Catedra

---
+++ Catedra/src/main/java/sv/edu/sv/Catedra/CatedraApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.sv.Catedra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatedraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatedraApplication.class, args);
+	}
+
+}
---
+++ Catedra/src/test/java/sv/edu/sv/Catedra/CatedraApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.sv.Catedra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatedraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
